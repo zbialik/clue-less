5e51d29bb828974eb71a66e2d416f7ed1d9754ff
Zachary Bialik <zachbialik1207@gmail.com>
2021-03-21 10:25:27 -0400

    updated with application.properties file

---
+++ src/main/java/clueless/ClueApplicationCorsConfig.java
@@ -0,0 +1,16 @@
+package clueless;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ 
+@Configuration
+@Profile("development")
+public class ClueApplicationCorsConfig implements WebMvcConfigurer {
+ 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+    }
+}
