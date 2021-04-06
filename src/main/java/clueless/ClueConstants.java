631282cacb074890deada7d6cc2017b39c69c8a5
Zachary Bialik <zachbialik1207@gmail.com>
2021-04-06 07:28:27 -0400

    put constants in separate interface

---
+++ src/main/java/clueless/ClueConstants.java
@@ -0,0 +1,72 @@
+package clueless;
+
+public interface ClueConstants {
+
+	// set card type constants
+	public static final String CARD_TYPE_WEAPON = "weapon";
+	public static final String CARD_TYPE_ROOM = "room";
+	public static final String CARD_TYPE_SUSPECT = "suspect";
+
+	// set location type constants
+	public static final String LOCATION_TYPE_ROOM = "room";
+	public static final String LOCATION_TYPE_HALLWAY = "hallway";
+	public static final String LOCATION_TYPE_HOME = "home";
+
+	// set card name constants
+	public static final String WEAPON_NAME_KNIFE = "knife";
+	public static final String WEAPON_NAME_REVOLVER = "revolver";
+	public static final String WEAPON_NAME_ROPE = "rope";
+	public static final String WEAPON_NAME_WRENCH = "wrench";
+	public static final String WEAPON_NAME_CANDLESTICK = "candlestick";
+	public static final String WEAPON_NAME_LEADPIPE = "leadpipe";
+
+	// set location name constants
+	public static final String LOCATION_NAME_BALL_ROOM = "ball room";
+	public static final String LOCATION_NAME_BILLIARD_ROOM = "billiard room";
+	public static final String LOCATION_NAME_CONSERVATORY = "conservatory";
+	public static final String LOCATION_NAME_DINING_ROOM = "dining room";
+	public static final String LOCATION_NAME_HALL = "hall";
+	public static final String LOCATION_NAME_KITCHEN = "kitchen";
+	public static final String LOCATION_NAME_LOUNGE = "lounge";
+	public static final String LOCATION_NAME_LIBRARY = "library";
+	public static final String LOCATION_NAME_STUDY = "study";
+	public static final String LOCATION_NAME_MRS_WHITE_HOME = "mrs. white home";
+	public static final String LOCATION_NAME_MR_GREEN_HOME = "mr. green home";
+	public static final String LOCATION_NAME_MRS_PEACOCK_HOME = "mrs. peacock home";
+	public static final String LOCATION_NAME_PROF_PLUM_HOME = "professor plum home";
+	public static final String LOCATION_NAME_MISS_SCARLET_HOME = "miss scarlet home";
+	public static final String LOCATION_NAME_COLONEL_MUSTARD_HOME = "colonel mustard home";
+	public static final String LOCATION_NAME_HALLWAY_32 = "hallway 32";
+	public static final String LOCATION_NAME_HALLWAY_52 = "hallway 52";
+	public static final String LOCATION_NAME_HALLWAY_23 = "hallway 23";
+	public static final String LOCATION_NAME_HALLWAY_43 = "hallway 43";
+	public static final String LOCATION_NAME_HALLWAY_63= "hallway 63";
+	public static final String LOCATION_NAME_HALLWAY_34 = "hallway 34";
+	public static final String LOCATION_NAME_HALLWAY_54 = "hallway 54";
+	public static final String LOCATION_NAME_HALLWAY_25 = "hallway 25";
+	public static final String LOCATION_NAME_HALLWAY_45 = "hallway 45";
+	public static final String LOCATION_NAME_HALLWAY_65 = "hallway 65";
+	public static final String LOCATION_NAME_HALLWAY_36 = "hallway 36";
+	public static final String LOCATION_NAME_HALLWAY_56 = "hallway 56";
+
+	// set character name constants
+	public static final String CHARACTER_NAME_MRS_WHITE = "mrs. white";
+	public static final String CHARACTER_NAME_MR_GREEN = "mr. green";
+	public static final String CHARACTER_NAME_MRS_PEACOCK = "mrs. peacock";
+	public static final String CHARACTER_NAME_PROF_PLUM = "professor plum";
+	public static final String CHARACTER_NAME_MISS_SCARLET = "miss scarlet";
+	public static final String CHARACTER_NAME_COLONEL_MUSTARD = "colonel mustard";
+	
+	// set player state constants
+	public static final String PLAYER_STATE_WAIT = "wait";
+	public static final String PLAYER_STATE_MOVE = "move";
+	public static final String PLAYER_STATE_SUGGEST = "suggest";
+	public static final String PLAYER_STATE_ACCUSE = "accuse";
+	public static final String PLAYER_STATE_REVEAL = "reveal";
+	public static final String PLAYER_STATE_AWAIT_REVEAL = "await_reveal";
+	public static final String PLAYER_STATE_ACCEPT_REVEAL = "accept_reveal";
+	public static final String PLAYER_STATE_COMPLETE_TURN = "complete_turn";
+	public static final String PLAYER_STATE_WIN = "win";
+	public static final String PLAYER_STATE_LOSE = "lose";
+	
+}
