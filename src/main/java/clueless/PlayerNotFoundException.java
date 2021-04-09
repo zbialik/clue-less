d0af85d18486a8fbe886e0e3d8e8ce7139a5e8f9
Zachary Bialik <zachbialik1207@gmail.com>
2021-04-09 12:25:01 -0400

    fixed exception handling

--- src/main/java/clueless/PlayerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package clueless;
-
-@SuppressWarnings("serial")
-public class PlayerNotFoundException extends RuntimeException {
-
-	public PlayerNotFoundException(String charName) {
-    super("Could not find player: " + charName);
-  }
-}
